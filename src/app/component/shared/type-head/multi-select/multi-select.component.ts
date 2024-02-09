12b55d04cfc8ba0f5bcf7b69a6747977488e18b8
Rahul <rahul.adsps@outlook.com>
2024-02-09 22:37:00 +0530

    multiselect working as expected

--- src/app/component/shared/type-head/multi-select/multi-select.component.ts
+++ src/app/component/shared/type-head/multi-select/multi-select.component.ts
@@ -1,8 +1,8 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { IFloatingInput, IType } from '../../../../../util/dataModel';
 import { NgClass } from '@angular/common';
-import { createContext } from 'vm';
+import { filterArrayFromArray } from '../../../../../util/transform';
 
 @Component({
   selector: 'sr-multi-select',
@@ -11,44 +11,39 @@ import { createContext } from 'vm';
   templateUrl: './multi-select.component.html',
   styleUrl: './multi-select.component.scss'
 })
-export class MultiSelectComponent implements OnInit {
-
+export class MultiSelectComponent implements OnChanges {
+  _data !: IType[]
+  @Output() selectItemEmmiter = new EventEmitter();
 
   @Input({ required: true }) CONTROL = new FormControl()
   @Input({ required: true }) CONSTANTS !: IFloatingInput
   @Input({ required: true }) data !: IType[]
   @Input({ required: true }) selectedTags: IType[] = []
-  @Output() selectItemEmmiter = new EventEmitter();
 
-  _searchResult: IType[] = []
-  _selectedItems: IType[] = []
-  _data: IType[] = []
+  searchResult: IType[] = []
 
-  ngOnInit(): void {
-    this._selectedItems = this.selectedTags
+  ngOnChanges(changes: SimpleChanges): void {
     this._data = this.data
+    this._data = filterArrayFromArray(this.data, this.selectedTags)
   }
 
   handleKeyUp($event: Event) {
     const element = ($event.target as HTMLInputElement)
     if (element.value.length < 2) {
-      this._searchResult = []
+      this.searchResult = []
     } else {
-      this._searchResult = this._data.filter(x => x.title.includes(element.value))
+      this.searchResult = this._data.filter(x => x.title.includes(element.value))
     }
   }
 
   handleSelect(tag: IType) {
-    if (this._selectedItems.includes(tag)) {
-      this._selectedItems = [...this._selectedItems.filter(x => x._id != tag._id)]
-
-      if (!this._data.includes(tag)) {
-        createContext
-      }
+    if (this.selectedTags.filter(x => x._id === tag._id).length > 0) {
+      this.selectedTags = this.selectedTags.filter(x => x._id !== tag._id)
     } else {
-      this._selectedItems = [...this._selectedItems, tag]
-      this._data = [...this._data.filter(x => x._id !== tag._id)]
+      this.selectedTags = [...this.selectedTags, tag]
+      this.searchResult = filterArrayFromArray(this.searchResult, [tag])
     }
-    this.selectItemEmmiter.emit(JSON.stringify(this._selectedItems))
+    this._data = filterArrayFromArray(this.data, this.selectedTags)
+    this.selectItemEmmiter.emit(JSON.stringify(this.selectedTags))
   }
-}
+} 
--- src/util/transform.ts
+++ src/util/transform.ts
@@ -1,3 +1,22 @@
+import { IType } from "./dataModel";
+
 export function addPer(value: string) {
     return `${value}vw`
 }
+
+export const filterArrayFromArray =
+    (_1: IType[], _2: IType[]): IType[] => {
+        _1.forEach(a => {
+            _2.forEach(b => {
+                if (b._id === a._id) {
+                    _1 = _1.filter(x => x._id !== b._id)
+                }
+            });
+        });
+        console.log('111111111', _1)
+        return _1
+    }
+
+
+    // data  always pass
+    // _data
