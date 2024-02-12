9f7c9df559e1711c38bf536ec4fb851f2fcb98d2
Rahul <rahul.adsps@outlook.com>
2024-02-12 14:10:43 +0530

    adding meta

--- src/app/component/page/blog-detail-page/blog-detail-page.component.ts
+++ src/app/component/page/blog-detail-page/blog-detail-page.component.ts
@@ -1,8 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MarkdownWrapperComponent } from '../../shared/markdown-wrapper.component';
 import { CommentComponent } from '../../shared/comment/comment.component';
 import { BlogDetailComponent } from '../../blog/blog-detail/blog-detail.component';
+import { Meta } from '@angular/platform-browser';
+import { IBlog } from '../../../../util/dataModel';
 
 @Component({
   selector: 'sr-blog-detail-page',
@@ -11,15 +13,32 @@ import { BlogDetailComponent } from '../../blog/blog-detail/blog-detail.componen
   templateUrl: './blog-detail-page.component.html',
   styleUrl: './blog-detail-page.component.scss'
 })
-export class BlogDetailPageComponent {
-  constructor(private activatedRoute: ActivatedRoute) { }
+export class BlogDetailPageComponent implements OnInit, OnDestroy {
+  constructor(private activatedRoute: ActivatedRoute, private meta: Meta) { }
 
-
-  blog: any
+  url = ''
+  blog!: IBlog
   ngOnInit() {
     this.activatedRoute.data.subscribe(
-      ({blogs}) => {
+      ({ blogs }) => {
         this.blog = blogs
       });
+    this.url = `http://sarkariresulthub.in${this.activatedRoute.snapshot.url.reduce((p: string, c) => p.concat('/' + c.path), '')}`
+
+    this.meta.addTags([
+      { property: 'og:url', content: this.url },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:title', content: this.blog.title },
+      { property: 'og:description', content: this.blog.description },
+      { property: 'og:image', content: this.blog.organisation.logo?.path || '' }
+    ])
+  }
+
+  ngOnDestroy(): void {
+    // this.meta.removeTag('og:url')
+    // this.meta.removeTag('og:title')
+    // this.meta.removeTag('og:title')
+    // this.meta.removeTag('og:description')
+    // this.meta.removeTag('og:image')
   }
 }
