0c5a8b5c2f1058a0ec05ec7e108fa22b566b04ef
Rahul <rahul.adsps@outlook.com>
2024-02-21 00:03:23 +0530

    image path copy is working as expected

--- src/app/component/file/file-item/file-item.component.ts
+++ src/app/component/file/file-item/file-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { IFile } from '../../../../util/dataModel';
-import { ToastService } from '../../../service/toast.service';
 
 @Component({
   selector: 'sr-file-item',
@@ -10,10 +9,9 @@ import { ToastService } from '../../../service/toast.service';
   styleUrl: './file-item.component.scss'
 })
 export class FileItemComponent {
-  constructor(private toastService: ToastService) {}
   @Input({required: true}) file !: IFile
 
-  copyImagePath(){
-    this.toastService.showSuccessToast('Success toast title', 'This is a success toast message.');
+  copyImagePath(filePath: string){
+    navigator.clipboard.writeText(filePath)
   }
 }
