1d3c5d9e066ec4d571b7741ca8128ef1c6f6313f
Rahul <rahul.adsps@outlook.com>
2024-02-21 19:46:08 +0530

    not working

--- src/app/component/file/create-file/create-file.component.ts
+++ src/app/component/file/create-file/create-file.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FloatingInputComponent } from '../../shared/type-head/floating-input/floating-input.component';
+import { FloatingTextareaComponent } from '../../shared/type-head/floating-textarea/floating-textarea.component';
+import { FILE_CONSTANT } from '../../../../util/constants';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FileService } from '../../../service/microservice/file.service';
+import { ToastService } from '../../../service/toast.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'sr-create-file',
   standalone: true,
-  imports: [],
+  imports: [FloatingInputComponent, FloatingTextareaComponent, ReactiveFormsModule],
   templateUrl: './create-file.component.html',
   styleUrl: './create-file.component.scss'
 })
-export class CreateFileComponent {
-
+export class CreateFileComponent implements OnInit {
+  FILE_CONSTANT = FILE_CONSTANT
+  id!: string;
+  defaultImage = 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/470.jpg'
+  fileForm;
+  constructor(private fb: FormBuilder, private activateRoute: ActivatedRoute, 
+    private fileService: FileService, private toastService: ToastService) {
+      
+    this.fileForm = this.fb.group({
+      name: ['', Validators.required],
+      path: ['', Validators.required],
+      description: ''
+    })
+  }
+  ngOnInit() {
+    this.id = this.activateRoute.snapshot.params['id']
+    if (this.id) {
+      this.fileService.get(this.id).subscribe((res) => {
+        this.fileForm.patchValue(res)
+      })
+    }
+  }
+  submitHandler() {
+    this.fileService.post(this.fileForm.value).subscribe((res) => {
+      this.toastService.showSuccessToast('Success', 'File created successfully')
+      this.fileForm.reset()
+    })
+  }
+  updateHandler() {
+    this.fileService.put(this.id, this.fileForm.value).subscribe((res) => {
+      this.toastService.showSuccessToast('Success', 'File updated successfully')
+      this.fileForm.reset()
+    })
+  }
 }
--- src/app/component/file/file-item/file-item.component.ts
+++ src/app/component/file/file-item/file-item.component.ts
@@ -1,10 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { IFile } from '../../../../util/dataModel';
+<<<<<<< Updated upstream
+=======
+import { ToastService } from '../../../service/toast.service';
+import { RouterLink } from '@angular/router';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'sr-file-item',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './file-item.component.html',
   styleUrl: './file-item.component.scss'
 })
